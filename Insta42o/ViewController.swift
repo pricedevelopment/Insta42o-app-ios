3282d1cd7bdd1a5ea21d4b268d3d8ea60d6edbed
President Price <newworldenterprices@newworldenterprices.com>
2018-01-23 02:51:00 -0800

    Initial Commit

---
+++ Insta42o/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Insta42o
+//
+//  Created by Chairman Price on 1/23/18.
+//  Copyright © 2018 Chairman Price. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
